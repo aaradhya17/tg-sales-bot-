07b870e4a4fea0b6e008a77d7412b1aff893a287
aaradhya17 <95566267+aaradhya17@users.noreply.github.com>
2026-06-07 00:46:44 +0530

    Update bot.py

--- telegram_sales_bot/bot.py
+++ telegram_sales_bot/bot.py
@@ -2,17 +2,15 @@ import tracemalloc
 tracemalloc.start()
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 from config import TOKEN, ADMIN_ID
-from handlers import start, menu, handle_callbacks, handle_screenshot
+from handlers import start, menu, handle_callbacks, handle_screenshot, handle_text
 
 def main():
     application = Application.builder().token(TOKEN).build()
-
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("menu", menu))
     application.add_handler(CallbackQueryHandler(handle_callbacks))
-
-    # Listens for screenshots sent by users
     application.add_handler(MessageHandler(filters.PHOTO, handle_screenshot))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
 
     application.run_polling()
 
