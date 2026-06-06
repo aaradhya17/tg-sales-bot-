8a14070d4f8056ef2f48b697bf04dd06bcd546e8
aaradhya17 <95566267+aaradhya17@users.noreply.github.com>
2026-06-06 16:11:32 +0530

    Update config.py

--- telegram_sales_bot/config.py
+++ telegram_sales_bot/config.py
@@ -1,21 +1,7 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-PAYMENT_TOKEN = os.getenv("PAYMENT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))  # Provide a default value of 0
-USDT_WALLET_ADDRESS = os.getenv("USDT_WALLET_ADDRESS")
-TON_API_KEY = os.getenv("TON_API_KEY")
-DB_PATH = "bot_database.db"
-
-import os
 
 TOKEN = os.environ.get("TOKEN", "").strip()
-ADMIN_ID = int(os.environ.get("ADMIN_ID", 0))
+ADMIN_ID = 6130754844
 
 if not TOKEN:
     raise ValueError("TOKEN environment variable is not set!")
-
-ADMIN_ID = 6130754844
